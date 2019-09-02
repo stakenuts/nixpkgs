1ee25a4cac5a087bafa948c723426d6dcf20f2c0
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-09-02 16:44:25 +0000

    gnome3.four-in-a-row: 3.32.0 → 3.34.0

--- pkgs/desktops/gnome-3/games/four-in-a-row/default.nix
+++ pkgs/desktops/gnome-3/games/four-in-a-row/default.nix
@@ -4,11 +4,11 @@
 
 stdenv.mkDerivation rec {
   pname = "four-in-a-row";
-  version = "3.32.0";
+  version = "3.34.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/four-in-a-row/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0h4wmbkdp7x3gp9sbxmvla316m8n6iy4f5sq0ksldj0z7ghlx9zl";
+    sha256 = "1wf0545lhmqf2hfi7wc8bqs7b9g43rm0df0ssza6517xzc4lx7jl";
   };
 
   nativeBuildInputs = [
