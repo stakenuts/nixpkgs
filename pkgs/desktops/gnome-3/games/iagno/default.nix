194bebbd3934728a211fdd64526f057ca02ae2e3
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-09-02 16:45:35 +0000

    gnome3.iagno: 3.32.0 → 3.34.0

--- pkgs/desktops/gnome-3/games/iagno/default.nix
+++ pkgs/desktops/gnome-3/games/iagno/default.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   pname = "iagno";
-  version = "3.32.0";
+  version = "3.34.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/iagno/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "1rcqb4gpam16xw87n4q2akkrg94ksrn16ry21pr6bsd7qs7hw17d";
+    sha256 = "15zan1h63hm9z0bf3y5h7rqd935jjw672qh88waqzwp6mlv1y321";
   };
 
   nativeBuildInputs = [
